830a5ce21ceece8d9c2f38a4f09168e1d9e7cbbb
hereistopdev <hereis.topdev@gmail.com>
2024-08-28 23:17:13 -0500

    fix download part

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -25,34 +25,38 @@ function App() {
   }, []);
 
   const getAccessToken = async () => {
-    try {
-      const response = await axios.post(
-        "https://egnyte-be.onrender.com/api/token",
-        {
-          grant_type: "password",
-          username: "hereis.topdev@gmail.com",
-          password: "Dr@gon0314",
-          client_id: "wtp8pzhmf63n6av27448wxn7", //'p9tpj7gvdzu6ea9szw4k3kvh',
-          client_secret: "bZPSVrN3euj4ygQzhZ7k35yEsxG9JmbbAVk5cNhqNjmsDuVnud", //'Eu28w2gyBtnBajtSmbt9QAbxBzmppbgyNhN9n8eyNxFMAuy2zx'
-        }
-      );
-      const accessToken = response.data.access_token;
-      console.log("Access Token:", accessToken);
+    const token = localStorage.getItem("access_token");
+    console.log(token);
+    if (!token || token.length == 0) {
+      try {
+        const response = await axios.post(
+          "https://egnyte-be.onrender.com/api/token",
+          {
+            grant_type: "password",
+            username: "hereis.topdev@gmail.com",
+            password: "Dr@gon0314",
+            client_id: "wtp8pzhmf63n6av27448wxn7", //'p9tpj7gvdzu6ea9szw4k3kvh',
+            client_secret: "bZPSVrN3euj4ygQzhZ7k35yEsxG9JmbbAVk5cNhqNjmsDuVnud", //'Eu28w2gyBtnBajtSmbt9QAbxBzmppbgyNhN9n8eyNxFMAuy2zx'
+          }
+        );
+        const accessToken = response.data.access_token;
+        console.log("Access Token:", accessToken);
 
-      // Store the token in localStorage
-      localStorage.setItem("access_token", accessToken);
-    } catch (error: any) {
-      if (error.response) {
-        // Server responded with a status code outside the range of 2xx
-        console.error("Response Error:", error.response.data);
-      } else if (error.request) {
-        // Request was made but no response received
-        console.error("Request Error:", error.request);
-      } else {
-        // Something happened in setting up the request
-        console.error("Error", error.message);
+        // Store the token in localStorage
+        localStorage.setItem("access_token", accessToken);
+      } catch (error: any) {
+        if (error.response) {
+          // Server responded with a status code outside the range of 2xx
+          console.error("Response Error:", error.response.data);
+        } else if (error.request) {
+          // Request was made but no response received
+          console.error("Request Error:", error.request);
+        } else {
+          // Something happened in setting up the request
+          console.error("Error", error.message);
+        }
+        console.error("Error Details:", error.config);
       }
-      console.error("Error Details:", error.config);
     }
   };
   return (
